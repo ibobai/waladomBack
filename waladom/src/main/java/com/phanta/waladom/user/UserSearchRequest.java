5c5f531351e31f25375e210988e45e7acc346287
Ibrahim Abuobaida <ibobai@Ibrahims-Mac-mini.local>
2025-02-10 21:45:49 +0100

    Search user by 5, and validate password

---
+++ waladom/src/main/java/com/phanta/waladom/user/UserSearchRequest.java
@@ -0,0 +1,50 @@
+package com.phanta.waladom.user;
+
+public class UserSearchRequest {
+    private String firstName;
+    private String lastName;
+    private Long id;
+    private String email;
+    private String phone;
+
+    // Getters and Setters
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
